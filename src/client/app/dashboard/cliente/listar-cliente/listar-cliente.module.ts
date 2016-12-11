bbc8aab78db8ef34a2829c99c2d45eb48efe3413
Rogério Cardoso <rogeriogerica@gmail.com>
2016-12-11 01:35:56 -0200

    Cliente - Cadastrar #8
    Paginação e typeahead

--- src/client/app/dashboard/cliente/listar-cliente/listar-cliente.module.ts
+++ src/client/app/dashboard/cliente/listar-cliente/listar-cliente.module.ts
@@ -1,14 +1,17 @@
+import { FiltroPorCliente } from './../pipes/cliente.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Ng2PaginationModule } from 'ng2-pagination'; // <-- import the module
 
 import {
     AlertModule,
     TabsModule,
     ModalModule,
     AccordionModule,
-    TypeaheadModule
+    TypeaheadModule,
+    PaginationModule
 } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { ListarClienteComponent } from './listar-cliente.component';
@@ -22,9 +25,11 @@ import { ListarClienteComponent } from './listar-cliente.component';
         TabsModule,
         ModalModule,
         AccordionModule,
-        TypeaheadModule
+        TypeaheadModule,
+        Ng2PaginationModule,
+        PaginationModule
     ],
-    declarations: [ListarClienteComponent],
+    declarations: [ListarClienteComponent, FiltroPorCliente],
     exports: [ListarClienteComponent]
 })
 
--- src/client/app/dashboard/vender-produto/vender-produto.module.ts
+++ src/client/app/dashboard/vender-produto/vender-produto.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import {
@@ -17,6 +17,7 @@ import { VenderProdutoComponent } from './vender-produto.component';
     imports: [
         BrowserModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule,
         AlertModule,
         TabsModule,
